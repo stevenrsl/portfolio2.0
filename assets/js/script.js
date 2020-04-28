f662b3981a199a55b5cf3a82065e610396271be3
stevenrsl <36017081+stevenrsl@users.noreply.github.com>
2020-04-29 00:13:19 +0200

    First Upload
    First upload with few changement in the code - few customization

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+$('#menu').on('click', function () {
+    $(this).toggleClass('menu-active');
+    $('#overlay').toggleClass('nav-active');
+});
+
+
+$(document).ready(function () {
+    // Declare Carousel jquery object
+    var owl = $('.owl-carousel');
+
+    // Carousel initialization
+    owl.owlCarousel({
+        mouseDrag: false,
+        animateOut: 'fadeOutDown',
+        animateIn: 'fadeInDown',
+        margin: 0,
+        loop: true,
+        navSpeed: 500,
+        smartSpeed: 2000,
+        dots: true,
+        nav: false,
+        autoplay: false,
+        rewind: false,
+        items: 1
+    });
+    // Custom Button
+    $('.NextBtn').click(function () {
+        owl.trigger('next.owl.carousel');
+    });
+    $('.PreviousBtn').click(function () {
+        owl.trigger('prev.owl.carousel');
+    });
+
+});

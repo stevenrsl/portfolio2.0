b57e09689b6603b0953c279351e89ddd4a942bdd
stevenrsl <36017081+stevenrsl@users.noreply.github.com>
2020-05-02 16:12:51 +0200

    Update 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,39 @@
-$('#menu').on('click', function () {
-    $(this).toggleClass('menu-active');
-    $('#overlay').toggleClass('nav-active');
+$('#menu').on('click', function() {
+  $(this).toggleClass('menu-active');
+  $('#overlay').toggleClass('nav-active');
 });
 
 
-$(document).ready(function () {
-    // Declare Carousel jquery object
-    var owl = $('.owl-carousel');
+$(document).ready(function() {
+// Declare Carousel jquery object
+var owl = $('.owl-carousel');
 
-    // Carousel initialization
-    owl.owlCarousel({
-        mouseDrag: false,
-        animateOut: 'fadeOutDown',
-        animateIn: 'fadeInDown',
-        margin: 0,
-        loop: true,
-        navSpeed: 500,
-        smartSpeed: 2000,
-        dots: true,
-        nav: false,
-        autoplay: false,
-        rewind: false,
-        items: 1
-    });
-    // Custom Button
-    $('.NextBtn').click(function () {
-        owl.trigger('next.owl.carousel');
-    });
-    $('.PreviousBtn').click(function () {
-        owl.trigger('prev.owl.carousel');
-    });
+// Carousel initialization
+owl.owlCarousel({
+mouseDrag: false,
+animateOut: 'fadeOutDown',
+animateIn: 'fadeInDown',
+margin: 0,
+loop: true,
+navSpeed: 500,
+smartSpeed: 2000,
+dots: true,
+nav: false,
+autoplay: false,
+rewind: false,
+items: 1
+});
+// Custom Button
+$('.NextBtn').click(function() {
+owl.trigger('next.owl.carousel');
+});
+$('.PreviousBtn').click(function() {
+owl.trigger('prev.owl.carousel');
+});
+$(".DisBtn").click(function() {
+  $("body").toggleClass("active");
+
+});
+
 
 });
